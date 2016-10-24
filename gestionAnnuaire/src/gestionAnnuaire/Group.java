317b4a5332bca2efa45808665e4f5d186227a3f9
Thomas <Thomas@192.168.1.14>
2016-10-24 11:57:16 +0200

    Création des JavaBeans

---
+++ gestionAnnuaire/src/gestionAnnuaire/Group.java
@@ -0,0 +1,27 @@
+package gestionAnnuaire;
+
+public class Group {
+	int idGroup;
+	String nameGroup;
+
+	public Group(){}
+
+	public int getIdGroup() {
+		return idGroup;
+	}
+	
+	public void setIdGroup(int idGroup) {
+		this.idGroup = idGroup;
+	}
+	
+	public String getNameGroup() {
+		return nameGroup;
+	}
+	
+	public void setNameGroup(String nameGroup) {
+		this.nameGroup = nameGroup;
+	}
+
+
+
+}
---
+++ gestionAnnuaire/src/gestionAnnuaire/Person.java
@@ -0,0 +1,58 @@
+package gestionAnnuaire;
+
+public class Person {
+	int idPers;
+	String firstName, lastName, mail, web, password;
+	
+	public Person(){}
+	
+	public int getId() {
+		return idPers;
+	}
+
+	public void setId(int id) {
+		this.idPers = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getWeb() {
+		return web;
+	}
+
+	public void setWeb(String web) {
+		this.web = web;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+}
